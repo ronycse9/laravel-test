aeee21eebe323a33ff20de288352458a3f02da27
Engr. Rony Debnath <ronycse9@gmail.com>
2017-08-16 01:12:38 +0600

    All kinds of route tested in web.php

---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+//normal route
+Route::get("/",function(){
+  return "Hello World!";
+});
+
+// match any 5 route method
+Route::match(['get','post','put','delete','options'],"/",function(){
+  return "Ok";
+});
+
+//any route method
+Route::any('/',function(){
+  return "HI";
+});
+
+// parameter must
+Route::get("/test/{id}",function($id){
+  return "My id is " . $id;
+});
+
+//parameter optional
+Route::get("/show/{id?}",function($id =  null){
+  return "My id is " . $id;
+});
+
+//multiple parameter route
+Route::get("/mul/{id}/{name}",function($id,$name){
+   return $id . "=>" .$name;
+});
+
+
+Route::get('user/profile', function () {
+    //
+})->name('profile');
